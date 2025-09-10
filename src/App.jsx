c1375d3d4a1251d61c828b8bd71ec8b03dbc7f5b
dxvarsh <borntobetwenty@gmail.com>
2025-09-10 23:15:50 +0530

    Project started and

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './index.css'
+function App() {
+
+  return (
+    <>
+      <div className="">
+        <h1 className="text-red-400 font-poppins dark:text-black">Admin</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
