c175ee52fcec6d383c927550f0bc750c460e98b1
dxvarsh <borntobetwenty@gmail.com>
2025-09-13 20:13:57 +0530

    LogInUIChanges: Login Page UI created.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,17 +1,37 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
+// /** @type {import('tailwindcss').Config} */
+// export default {
+//     content: [
+//       "./index.html",
+//       "./src/**/*.{js,ts,jsx,tsx}",
+//     ],
+//     theme: {
+//       extend: {},
+//     },
+//   }
+
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      // Add custom colors, fonts, or breakpoints here
+      colors: {
+        'default': '#ffffff',
+        'subtle': '#666666',
+        'muted': '#999999',
+        'darker': '#333333',
+        'state-soft': '#f0f0f0',
+        'state-soft-hover': '#e0e0e0',
+        'state-soft-press': '#d0d0d0',
+      },
     },
-    plugins: [],
-    darkMode: 'class',
-    // safelist: [
-    //   {
-    //     pattern: /(text|bg|border|hover)/,
-    //   },
-    // ]
-  }
+  },
+  plugins: [],
+  darkMode: 'class',
+  // safelist: [
+  //   {
+  //     pattern: /(text|bg|border|hover)/,
+  //   },
+  // ]
+}
