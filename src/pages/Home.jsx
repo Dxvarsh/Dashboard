7f4318847e9d44caa147353707cd16a7e5862977
Devarsh Nagar <140646751+Dxvarsh@users.noreply.github.com>
2025-09-17 09:37:36 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,8 +5,8 @@ const Home = () => {
   return (
     <div className='w-full h-screen flex align-center justify-center flex-col gap-2'>
       <h1 className='text-2xl font-bold'>Home</h1>
-      <NavLink to="/login">Login</NavLink>
-      <NavLink to="/admin/dashboard">Dashboard</NavLink>
+      <NavLink to="Dashboard/login">Login</NavLink>
+      <NavLink to="Dashboard/admin/dashboard">Dashboard</NavLink>
     </div>
   )
 }
